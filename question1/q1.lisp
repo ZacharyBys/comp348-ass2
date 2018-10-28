b8650f103b0ed1343f9c493a6402b04277a4bea8
ZacharyBys <zachary.bys@gmail.com>
2018-10-28 19:32:47 -0400

    Fixed question 1 string input

--- question1/q1.lisp
+++ question1/q1.lisp
@@ -1,19 +1,29 @@
 (defun triangle (n)
-    (if (integerp n)
-        (loop for x from 1 to n
-            collect (loop for y from 1 to x
-                if (> y 1)
-                do (
-                    format t "~A " (write-to-string y)
-                )
-                else do (
-                    print y
+    (cond (
+        (integerp n)
+            (loop for x from 1 to n
+                collect (loop for y from 1 to x
+                    if (> y 1)
+                    do (
+                        format t "~A " (write-to-string y)
+                    )
+                    else do (
+                        print y
+                    )
                 )
             )
         )
-        (print "decimal numbers are not valid input, please enter an integer")
+        (
+            (numberp n)
+            (print "decimal numbers are not valid input, please enter an integer")
+        )
+        (
+            t
+            (print "strings numbers are not valid input, please enter an integer")
+        )
     )
 )
 
 (triangle 5)
 (triangle 2.5)
+(triangle "2.5")
